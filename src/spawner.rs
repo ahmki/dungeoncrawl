c7e78040931dfc6744436a0c5bbfd4173b52e243
ahmki <aleksi.heinimaki1@gmail.com>
2022-07-08 14:22:17 +0300

    Health and tooltip systems

--- src/spawner.rs
+++ src/spawner.rs
@@ -11,7 +11,8 @@ pub fn spawn_player(
             Render{
                 color: ColorPair::new(WHITE, BLACK),
                 glyph : to_cp437('@')
-            }
+            },
+            Health{ current: 20, max: 20 }
         )
     );
 }
@@ -21,6 +22,12 @@ pub fn spawn_monster(
     rng: &mut RandomNumberGenerator,
     pos : Point
 ) {
+
+    let (hp, name, glyph) = match rng.roll_dice(1, 10) {
+        1..=8 => goblin(),
+        _ => orc()
+    };
+
     ecs.push(
         (
             Enemy,
@@ -34,7 +41,16 @@ pub fn spawn_monster(
                     _ => to_cp437('g'),
                 }
             },
-            MovingRandomly{}
+            MovingRandomly{},
+            Health{current: hp, max: hp},
+            Name(name)
         )
     );
+}
+
+fn goblin() -> (i32, String, FontCharType) {
+    (1, "Goblin".to_string(), to_cp437('g'))
+}
+fn orc() -> (i32, String, FontCharType) {
+    (2, "Orc".to_string(), to_cp437('o'))
 }
